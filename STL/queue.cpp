213bb1a71f1b4e55c19a4ae675fc15363ecd551a
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-25 00:15:15 +0530

    DSA

---
+++ STL/queue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main(){
+    queue<string>q;
+
+    q.push("Aryan");
+    q.push("Mittal");
+
+    cout<<"first element"<<" "<<q.front()<<endl;
+
+    cout<<"size"<<q.size()<<endl;
+}
