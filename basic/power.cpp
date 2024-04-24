e132415413df3d688f2ce7d34e52aef8f82bbfe2
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-25 00:17:03 +0530

    DSA

---
+++ basic/power.cpp
@@ -0,0 +1,10 @@
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result ;
+        result = pow(x,n);
+        return result;
+    }
+};
