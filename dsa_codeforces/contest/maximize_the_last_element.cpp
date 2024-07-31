b399f2c05290e05b3371d1121bebff906173b280
Aryanmittal23 <mittalaryan932@gmail.com>
2024-07-31 16:50:11 +0530

    dsa

---
+++ dsa_codeforces/contest/maximize_the_last_element.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+       int n;
+       cin>>n;
+       int arr[n];
+
+       for(int i=0 ;i<n ;i++ ){
+        cin>>arr[i];
+       }
+       int maxi = INT_MIN;
+       for(int i=0 ;i<n ;i=i+2){
+            maxi = max(maxi,arr[i]);
+    
+       }
+       cout<<maxi<<endl;
+    } 
+    return 0;
+}
